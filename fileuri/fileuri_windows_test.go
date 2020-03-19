5afeec08f447b93561341d5faaf7124fcd8f0153
Neil Pankey <npankey@gmail.com>
2020-03-18 17:55:27 -0700

    fileuri: Basic tests

--- fileuri/fileuri_windows_test.go
+++ fileuri/fileuri_windows_test.go
@@ -4,4 +4,24 @@ import (
 	"testing"
 )
 
-func TestFromPath(t *testing.T) { t.Errorf("TODO") }
+func TestFromPath(t *testing.T) {
+	tests := []struct{
+		in, out string
+	} {
+		{ `C:`, "file:///C:" },
+		{ `C:\foo\bar.txtt`, "file:///C:/foo/bar.txt" },
+		{ `\\host\path\file.ext`, "file://host/path/file.ext" },
+	}
+	for _, tt := range tests {
+		t.Run(tt.in, func(t *testing.T) {
+			uri, err := FromPath(tt.in)
+			if err != nil {
+				t.Errorf("unexpected error: %s", err)
+				return
+			}
+			if uri != tt.out {
+				t.Errorf("got: %s, want: %s", uri, tt.out)
+			}
+		})
+	}
+}
