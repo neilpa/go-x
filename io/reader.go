a9d24416908e4d55970521536cb33e057809476e
Neil Pankey <npankey@gmail.com>
2020-03-24 13:52:38 -0700

    xio: New package and w/ ScannableReader
    Adds a workaround for io.Reader implementations that don't behave
    correctly in error conditions, e.g. return negative values for bytes
    read.

---
+++ io/reader.go
@@ -0,0 +1,29 @@
+// Package xio provides extensions and hacks on top of io from the stdlib
+package xio
+
+import (
+	"io"
+)
+
+// ScannableReader wraps poorly behaved io.Reader implementations to make them
+// safe to use with bufio.Scanner. In particular, when the underlying reader
+// returns an error, it ensures that negative values for bytes read don't bubble
+// out. Instead, this will return 0 along with the original error. One source of
+// such bugs returning values directly from golang.org/x/sys/unix.Read.
+func ScannableReader(r io.Reader) io.Reader {
+	return scannableReader{r}
+}
+
+type scannableReader struct {
+	r io.Reader
+}
+
+// Read implements io.Reader and "fixes" poorly behaved implementations during
+// error conditions
+func (r scannableReader) Read(p []byte) (n int, err error) {
+	n, err = r.r.Read(p)
+	if err != nil && n < 0 {
+		n = 0
+	}
+	return
+}
---
+++ io/reader_test.go
@@ -0,0 +1,38 @@
+package xio
+
+import (
+	"bufio"
+	"errors"
+	"testing"
+)
+
+func TestScannableReader(t *testing.T) {
+	lines := []string{ "one", "two", "three" }
+	r := &badReader{lines, errors.New("bad")}
+	scanner := bufio.NewScanner(ScannableReader(r))
+	for i := 0; scanner.Scan(); i++ {
+		got := scanner.Text()
+		if lines[i] != got {
+			t.Errorf("scan @%d: got %q want %q", i, got, lines[i])
+		}
+	}
+	if err := scanner.Err(); err != r.err {
+		t.Errorf("scan err: got %q want %q", err, r.err)
+	}
+}
+
+type badReader struct {
+	lines []string
+	err error
+}
+
+func (r *badReader) Read(p []byte) (n int, err error) {
+	if len(r.lines) > 0 {
+		n = copy(p, []byte(r.lines[0] + "\n"))
+		r.lines = r.lines[1:]
+	} else {
+		n = -1
+		err = r.err
+	}
+	return
+}
