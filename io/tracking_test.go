49bae5d8a5b14ddd2825bc36d7d201ce3792b838
Neil Pankey <npankey@gmail.com>
2020-03-25 17:29:49 -0700

    xio: TrackingReader test notes

--- io/tracking_test.go
+++ io/tracking_test.go
@@ -5,4 +5,10 @@ import (
 )
 
 func TestTrackingReader(t *testing.T) { // TODO
+	// Read basics
+	// Seek wrapping io.ReadSeeker works
+	// Seek wrapping io.Reader
+	//	- works from current
+	//	- may work from start
+	//	- always fails from end
 }
