ba5e2db27d6a5fcaedccf771af028cf9fbfc2dc2
Neil Pankey <npankey@gmail.com>
2020-03-25 19:51:07 -0700

    xio: TrackingReader: validate read afteer seek

--- io/tracking_test.go
+++ io/tracking_test.go
@@ -2,6 +2,7 @@ package xio
 
 import (
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
@@ -35,46 +36,60 @@ func ExampleTrackingReader() {
 func TestTrackingReader_Seek(t *testing.T) {
 	text := "some text"
 
-	tests := []struct{
+	tests := []struct {
 		skip int64
 
-		seek int64
+		seek   int64
 		whence int
 
 		offset int64
-		fail bool
-	} {
-		{ 0, 0, io.SeekCurrent, 0, false },
-		{ 0, 0, io.SeekStart, 0, false },
+		fail   bool
+	}{
+		{0, 0, io.SeekCurrent, 0, false},
+		{0, 0, io.SeekStart, 0, false},
 
-		{ 1, 0, io.SeekCurrent, 1, false },
-		{ 1, 1, io.SeekCurrent, 2, false },
+		{1, 0, io.SeekCurrent, 1, false},
+		{1, 1, io.SeekCurrent, 2, false},
 
-		{ 1, 2, io.SeekStart, 2, false },
-		{ 1, 1, io.SeekStart, 1, false },
-		{ 1, 0, io.SeekStart, 1, true },
+		{1, 2, io.SeekStart, 2, false},
+		{1, 1, io.SeekStart, 1, false},
+		{1, 0, io.SeekStart, 1, true},
 
-		{ 2, -1, io.SeekCurrent, 2, true},
+		{2, -1, io.SeekCurrent, 2, true},
 
-		{ 0, 0, io.SeekEnd, 0, true },
-		{ 5, 0, io.SeekEnd, 5, true },
-		{ int64(len(text)), -2, io.SeekEnd, int64(len(text)), true },
+		{0, 0, io.SeekEnd, 0, true},
+		{5, 0, io.SeekEnd, 5, true},
+		{int64(len(text)), -2, io.SeekEnd, int64(len(text)), true},
 	}
 
 	for i, tt := range tests {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
-			r := NewTrackingReader(reader{strings.NewReader(text)})
-			r.pos = tt.skip
+			sr := strings.NewReader(text)
+			r := NewTrackingReader(reader{sr})
+			if tt.skip > 0 {
+				if _, err := sr.Seek(tt.skip, io.SeekCurrent); err != nil {
+					t.Fatalf("strings reader: %s", err)
+				}
+				r.pos = tt.skip
+			}
 
 			off, err := r.Seek(tt.seek, tt.whence)
 			if err != nil && !tt.fail {
-				t.Errorf("seek: unexpected failure %s", err)
+				t.Errorf("seek: unexpected failure: %s", err)
 			}
 			if off != tt.offset {
 				t.Errorf("offset: got %d want %d", off, tt.offset)
 			}
 			if r.Offset() != tt.offset {
 				t.Errorf("reader: got %d want %d", r.Offset(), tt.offset)
+			}
+
+			tail, err := ioutil.ReadAll(r)
+			if err != nil {
+				t.Errorf("read: unexpected failure: %s", err)
+			}
+			if string(tail) != text[off:] {
+				t.Errorf("read: got %q want %q", string(tail), text[off:])
 			}
 		})
 	}
