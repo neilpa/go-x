849972322f5b12aa7073abe90872c19a2772c1ac
Neil Pankey <git@neilpa.me>
2023-05-12 07:35:42 -0700

    goexpr: always write AST

--- cmd/goexpr/goexpr.go
+++ cmd/goexpr/goexpr.go
@@ -1,7 +1,7 @@
-// goexpr attempts to parse each command line argument as a go expresions. This
-// is a basic wrapper around `go/parser.ParseExpr`. Each successfully parsed
-// argument has it's AST written to stdout. Errors for expressions that fail to
-// parse are written to stderr.
+// goexpr attempts to parse each command line argument as a go expression. This
+// is a basic wrapper around `go/parser.ParseExpr`. Each argument has it's AST
+// written to stdout, even if `ParseExpr` returns an error. The status of the
+// parse result is written to stderr for each expression as well.
 package main
 
 import (
@@ -19,11 +19,12 @@ func main() {
 	}
 	for _, arg := range args {
 		expr, err := parser.ParseExpr(arg)
-		if err == nil {
-			fmt.Printf("goexpr: %s:\n", arg)
-			ast.Print(nil, expr)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "goexpr:fail:", arg)
+			fmt.Fprintln(os.Stderr, "goexpr:error:", err)
 		} else {
-			fmt.Fprintf(os.Stderr, "goexpr: %s: %v\n", arg, err)
+			fmt.Fprintln(os.Stderr, "goexpr:pass:", arg)
 		}
+		ast.Print(nil, expr)
 	}
 }
